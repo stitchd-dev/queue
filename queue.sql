c114db12a4d5b30ffa28891d0980787812f7231b
Vishal Kumar <vkgkrt@gmail.com>
2025-11-10 19:31:57 +0530

    Initialize Rust project with PostgreSQL connection, async runtime, and IntelliJ configurations.

---
+++ queue.sql
@@ -0,0 +1,170 @@
+-- Cleanup
+drop table if exists queue;
+drop table if exists destination;
+do
+$$
+    declare
+        r record;
+    begin
+        for r in (select tablename
+                  from pg_tables
+                  where schemaname = 'public'
+                    and tablename ~ '^queue_[0-9]+_data_[0-9]+$')
+            loop
+                execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+            end loop;
+    end
+$$;
+do
+$$
+    declare
+        r record;
+    begin
+        for r in (select tablename
+                  from pg_tables
+                  where schemaname = 'public'
+                    and tablename ~ '^queue_[0-9]+_job_[0-9]+$')
+            loop
+                execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+            end loop;
+    end
+$$;
+drop table if exists source;
+drop routine if exists add_destination;
+drop routine if exists create_dataset;
+drop routine if exists update_status;
+drop type if exists failed_job_update;
+drop type if exists job_status;
+
+-- Setup
+create table destination
+(
+    id     serial primary key,
+    name   varchar(255) not null,
+    type   varchar(255) not null,
+    config jsonb
+);
+
+create table source
+(
+    id   serial primary key,
+    name varchar(255) not null,
+    type varchar(255) not null
+);
+
+create table queue
+(
+    id                  serial primary key,
+    destination_id      int references destination (id) not null,
+    created_at          timestamp                       not null default now(),
+    updated_at          timestamp                       not null default now(),
+    current_dataset     int                             not null default 0
+        constraint queue_current_dataset_check check (current_dataset >= 0),
+    processing_dataset  int                             not null default 0
+        constraint queue_processing_dataset_check check ( processing_dataset >= 0 and processing_dataset <= current_dataset ),
+    last_failed_dataset int                             not null default 0
+        constraint queue_last_failed_dataset_check check ( last_failed_dataset >= 0 and last_failed_dataset <= processing_dataset )
+);
+
+create index queue_destination_id_idx on queue (destination_id);
+
+create type job_status as enum ('pending', 'processing', 'failed', 'done', 'failed_permanently');
+
+create or replace function create_dataset(queue_id int, dataset_id int) returns void as
+$$
+declare
+    data_table_name   text;
+    job_table_name    text;
+    status_index_name text;
+    try_at_index_name text;
+begin
+    data_table_name := format('queue_%s_data_%s', queue_id, dataset_id);
+    job_table_name := format('queue_%s_job_%s', queue_id, dataset_id);
+    status_index_name := format('queue_%s_job_%s_status_idx', queue_id, dataset_id);
+    try_at_index_name := format('queue_%s_job_%s_try_at_idx', queue_id, dataset_id);
+
+
+    execute format(
+            'create table %I (
+                id     serial primary key,
+                data   jsonb not null,
+                source int references source (id) not null
+            )',
+            data_table_name);
+    execute format(
+            'create table %I (
+                id         serial primary key,
+                status     job_status not null default ''pending'',
+                data       int references %I (id) not null,
+                try_at     timestamp not null default now(),
+                retry_count int not null default 0,
+                created_at timestamp not null default now(),
+                updated_at timestamp not null default now()
+            )',
+            job_table_name, data_table_name);
+
+
+    execute format('create index %I on %I (status)', status_index_name, job_table_name);
+    execute format('create index %I on %I (try_at)', try_at_index_name, job_table_name);
+end;
+$$ language plpgsql;
+
+create or replace function add_destination(name varchar(255), type varchar(255), config jsonb) returns int as
+$$
+declare
+    destination_id int;
+    queue_id       int;
+    dataset_id     int;
+begin
+    insert into destination (name, type, config) values (name, type, config) returning id into destination_id;
+    insert into queue (destination_id) values (destination_id) returning id, current_dataset into queue_id, dataset_id;
+
+    perform create_dataset(queue_id, dataset_id);
+
+    return destination_id;
+end;
+$$ language plpgsql;
+
+create type failed_job_update as
+(
+    id          int,
+    try_at      timestamp,
+    retry_count int
+);
+
+create or replace function update_status(queue_id int, dataset_id int, done_jobs int[],
+                                         failed_permanently_jobs int[], failed_jobs failed_job_update[]) returns void as
+$$
+declare
+    table_name text;
+begin
+    table_name := format('queue_%s_job_%s', queue_id, dataset_id);
+
+    if array_length(done_jobs, 1) > 0 then
+        execute format('update %I set
+                           status = ''done'',
+                           updated_at = now()
+                       where id = any($1)',
+                       table_name) using done_jobs;
+    end if;
+
+    if array_length(failed_permanently_jobs, 1) > 0 then
+        execute format('update %I set
+                            status = ''failed_permanently'',
+                            updated_at = now()
+                       where id = any($1)',
+                       table_name) using failed_permanently_jobs;
+    end if;
+
+    if array_length(failed_jobs, 1) > 0 then
+        execute format(
+                'update %I set
+                    status = ''failed'',
+                    updated_at = now(),
+                    try_at = f.try_at,
+                    retry_count = f.retry_count
+                from unnest($1) as f where %I.id = f.id',
+                table_name) using failed_jobs;
+    end if;
+end;
+$$ language plpgsql;
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use deadpool::Runtime;
+use deadpool_postgres::tokio_postgres::NoTls;
+use deadpool_postgres::{Config, ManagerConfig, RecyclingMethod};
+
+#[tokio::main]
+async fn main() {
+    let mut config = Config::new();
+    config.dbname = Some("vishal".to_string());
+    config.user = Some("vishal".to_string());
+    config.password = Some("password".to_string());
+    config.host = Some("localhost".to_string());
+    config.port = Some(5432);
+
+    config.manager = Some(ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+    });
+
+    let pool = config.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+
+    println!("{:?}", pool);
+
+    for i in 1..10i32 {
+        let client = pool.get().await.unwrap();
+        let stmt = client.prepare_cached("SELECT 1 + $1").await.unwrap();
+        let rows = client.query(&stmt, &[&i]).await.unwrap();
+        let value: i32 = rows[0].get(0);
+        assert_eq!(value, i + 1);
+    }
+}
